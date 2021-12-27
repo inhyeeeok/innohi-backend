c3cc726d09fe48764d3af2ac50c7bf20f2c847f6
inhyeeeok <nangmanvv@hanmail.net>
2021-12-27 15:10:39 +0900

    폴더 구조 변경에 따른 basePackage 값 변경

--- src/main/java/com/hist/innohi/Swagger2Config.java
+++ src/main/java/com/hist/innohi/Swagger2Config.java
@@ -17,7 +17,7 @@ public class Swagger2Config {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+				.apis(RequestHandlerSelectors.basePackage("com.hist.innohi"))
 				.paths(PathSelectors.any())
 				.build();
 	}
