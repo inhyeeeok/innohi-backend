db44520cb9276c12c500ef71d056c44e00645835
inhyeeeok <nangmanvv@hanmail.net>
2022-01-03 13:29:07 +0900

    불필요한 import 제거

--- src/main/java/com/hist/innohi/persistence/BoardRepository.java
+++ src/main/java/com/hist/innohi/persistence/BoardRepository.java
@@ -1,13 +1,10 @@
 package com.hist.innohi.persistence;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hist.innohi.model.BoardEntity;
-import com.hist.innohi.model.StartUpEntity;
 
-import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, String>{
